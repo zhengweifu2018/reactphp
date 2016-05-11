2514fc59ea368c826f4c1a2cee5882bcac863457
zhengweifu <zhengweifu@gdi.com.cn>
2016-05-11 15:34:06 +0800

    index.bundle.js mini file

--- Public/webpack.config.js
+++ Public/webpack.config.js
@@ -2,46 +2,51 @@
  var path = require('path');
 
 module.exports = {
-    // cache: true,
+  // cache: true,
 
-    entry: {
-        index: './src/home/index/index.js'
-    },
+  entry: {
+    index: './src/home/index/index.js'
+  },
 
-    output: {
-        path: __dirname + '/dist',
-        filename: '[name].bundle.js'
-    },
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].bundle.js'
+  },
 
-    resolve: {
-        extensions: ['', '.js', '.jsx', '.css', '.scss', 'jpg', 'png']
-    },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.css', '.scss', 'jpg', 'png']
+  },
 
-    externals: {
-        'three': 'THREE',
-    },
+  externals: {
+    'three': 'THREE',
+  },
 
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel', // 'babel-loader' is also a legal name to reference
-                query: {
-                    presets: ['es2015', 'react', 'stage-0']
-                }
-            }, {
-                test: /\.scss$/,
-                loader: 'style!css!sass'
-            }, {
-                test: /\.(jpg|png)$/,
-                loader: 'url?limit=8192?name=dist/[hash:8].[name].[ext]'
-            }
-        ]
-    },
-
-    plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.NoErrorsPlugin()
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel', // 'babel-loader' is also a legal name to reference
+        query: {
+          presets: ['es2015', 'react', 'stage-0']
+        }
+      }, {
+        test: /\.scss$/,
+        loader: 'style!css!sass'
+      }, {
+        test: /\.(jpg|png)$/,
+        loader: 'url?limit=8192?name=dist/[hash:8].[name].[ext]'
+      }
     ]
+  },
+
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    })
+  ]
 };
